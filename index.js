a7d25c80dcac6a82a761d97e43c6951498e9f6d1
Nugget1368 <rneg1@hotmail.com>
2024-11-28 12:57:31 +0100

    Remove task function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const addTodo = () => {
   const todoList = document.querySelector("ul#todo-list");
   const todoInput = document.querySelector("input#todo-input");
   const todo = document.createElement("li");
-  todo.innerHTML = `<div><input type="checkbox" id="${todoInput.value}"/> <label for="${todoInput.value}"> ${todoInput.value}</label></div>`;
+  todo.innerHTML = `<div><input type="checkbox" id="${todoInput.value}"/> <label for="${todoInput.value}"> ${todoInput.value}</label></div><div class="btn-container"><button class="remove-todo">X</button></div>`;
   todoList.append(todo);
   //   updateCheckboxes();
   let checkboxes = document.querySelectorAll(
@@ -18,6 +18,12 @@ const addTodo = () => {
           : "none")
     )
   );
+  let removeBtns = document.querySelectorAll(
+    'ul#todo-list button.remove-todo'
+  );
+  removeBtns.forEach((btn) => btn.addEventListener("click", (event) => {
+      event.target.closest("li").remove();
+  }));
 };
 
 document.querySelector("button#add-todo").addEventListener("click", addTodo);
